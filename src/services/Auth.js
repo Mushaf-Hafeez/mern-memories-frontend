c13d47f67e13b117f1ef49d178a163ae93238aa0
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-07-29 12:56:23 +0500

    fix: change the backend path in auth.js

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,56 +1,61 @@
 import axios from "axios";
 
-export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-})
+const backend_url = import.meta.env.VITE_BACKEND_URL + "/api/v1";
 
+export const api = axios.create({
+  baseURL: backend_url,
+});
 
 api.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')) : null
-    if (token) {
-        req.headers.Authorization = `Bearer ${token}`
-    }
+  const token = localStorage.getItem("token")
+    ? JSON.parse(localStorage.getItem("token"))
+    : null;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return req
-})
+  return req;
+});
 
 export const login = async (email, password) => {
-    try {
-        const response = await api.post('/login', {
-            email,
-            password
-        })
+  try {
+    const response = await api.post("/login", {
+      email,
+      password,
+    });
 
-        // console.log(response.data)
-        return response.data
+    // console.log(response.data)
+    return response.data;
+  } catch (error) {
+    console.log("Error Message: ", error.message);
+    console.log("Error Status: ", error.response.status);
+    console.log("Error Data: ", error.response.data);
+    return error.response.data;
+  }
+};
 
-    } catch (error) {
-        console.log('Error Message: ', error.message)
-        console.log('Error Status: ', error.response.status)
-        console.log('Error Data: ', error.response.data)
-        return error.response.data
-    }
-}
+export const signin = async (
+  firstName,
+  lastName,
+  email,
+  password,
+  confirmPassword
+) => {
+  try {
+    const response = await api.post("/signin", {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+    });
 
-export const signin = async (firstName, lastName, email, password, confirmPassword) => {
-    try {
-
-        const response = await api.post('/signin', {
-            firstName,
-            lastName,
-            email,
-            password,
-            confirmPassword
-        })
-
-        // console.log('Signin response: ', response.data)
-        return response.data
-
-    } catch (error) {
-        console.log('Error Message: ', error.message)
-        console.log('Error Status: ', error.response.status)
-        console.log('Error Data: ', error.response.data)
-        return error.response.data
-    }
-}
-
+    // console.log('Signin response: ', response.data)
+    return response.data;
+  } catch (error) {
+    console.log("Error Message: ", error.message);
+    console.log("Error Status: ", error.response.status);
+    console.log("Error Data: ", error.response.data);
+    return error.response.data;
+  }
+};
