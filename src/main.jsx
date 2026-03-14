94a52dcdcdd624bd0e2e108c5f694185a477ac47
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2026-03-14 12:18:58 +0500

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,29 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from 'react-hot-toast'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store/store.js'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store/store.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <Provider store={store}>
       <BrowserRouter>
         <App />
-        <Toaster toastOptions={{
-          style: {
-            borderRadius: '8px',
-            background: '#1F1F27',
-            color: '#fff',
-          },
-        }} />
+        <Analytics />
+        <Toaster
+          toastOptions={{
+            style: {
+              borderRadius: "8px",
+              background: "#1F1F27",
+              color: "#fff",
+            },
+          }}
+        />
       </BrowserRouter>
     </Provider>
-  </GoogleOAuthProvider>
-
-)
+  </GoogleOAuthProvider>,
+);
